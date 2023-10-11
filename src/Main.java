743c980dd53d0679ef920e26ccf7907e13388582
nibchee <nirbhaybansal28@gmail.com>
2023-10-11 22:43:48 +0530

    Threading added

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        while(true){
-            Scanner sc=new Scanner(System.in);
+        while (true) {
+            Scanner sc = new Scanner(System.in);
             System.out.println("Pls enter n to get nth prime");
-            int n=sc.nextInt();
-            if(n==0)
+            int n = sc.nextInt();
+            if (n == 0)
                 break;
-            int number=PrimeNumberUtil.calculate(n);
-            System.out.println("Value of nth is "+number);
+
+            //creating thread for each new request
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    int number = PrimeNumberUtil.calculate(n);
+                    System.out.println("Value of " + n + "th is " + number);
+                }
+            };
+
+            Thread t = new Thread(r);
+            t.start();
+
         }
     }
 }
