31c799a1b0719338616d71addba9ce0abff954d0
nibchee <nirbhaybansal28@gmail.com>
2023-10-11 22:50:45 +0530

    Threading with states & Sleep() added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<Thread> threads=new ArrayList<>();
         while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.println("Pls enter n to get nth prime");
@@ -19,8 +22,29 @@ public class Main {
             };
 
             Thread t = new Thread(r);
+            threads.add(t);
             t.start();
-
         }
+
+        Runnable statusReporter=()->{
+            while(true){
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                printThreads(threads);
+            }
+        };
+        Thread reporterThread=new Thread(statusReporter);
+        reporterThread.start();
+
+    }
+
+    public static void printThreads(List<Thread> threads){
+        System.out.println("\n Curently "+threads.size()+" threads runnning with status:");
+        threads.forEach(thread -> {
+            System.out.print(thread.getState()+" ");
+        });
     }
 }
