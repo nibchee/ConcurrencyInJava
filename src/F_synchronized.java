740bb2aac0f454d793e39a11e9fce3de836774ae
nibchee <nirbhaybansal28@gmail.com>
2023-10-12 23:58:46 +0530

    Multithreading Program without synchronization

---
+++ src/F_synchronized.java
@@ -0,0 +1,41 @@
+
+
+class Counter implements Runnable {
+    private int value = 0;
+
+    public void increment() {
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        value++;
+    }
+
+
+    public void decrement() {
+        value--;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void run() {
+        this.increment();
+        System.out.println(Thread.currentThread().getName() + " increments: " + this.getValue());
+        this.decrement();
+        System.out.println(Thread.currentThread().getName() + " decrements: " + this.getValue());
+    }
+}
+
+
+public class F_synchronized {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        new Thread(counter, "one").start();
+        new Thread(counter, "two").start();
+        new Thread(counter, "Three").start();
+        new Thread(counter, "Four").start();
+    }
+}
