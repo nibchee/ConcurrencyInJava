882763172153a7c3e1b4295f33fc243d81d5a8b0
nibchee <nirbhaybansal28@gmail.com>
2023-10-13 20:29:19 +0530

    Lock CLass used

--- src/F_synchronized.java
+++ src/F_synchronized.java
@@ -1,8 +1,10 @@
-
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Counter implements Runnable {
     private int value = 0;
-    private Integer i=10;
+    private Integer i = 10;
+    private Lock l = new ReentrantLock();
 
     public void increment() {
         try {
@@ -23,12 +25,19 @@ class Counter implements Runnable {
     }
 
     public void run() {
-        synchronized (this) {
+        // get a lock
+        //  synchronized (this) {
+        l.lock();
+        try {
             this.increment();
             System.out.println(Thread.currentThread().getName() + " increments: " + this.getValue());
             this.decrement();
             System.out.println(Thread.currentThread().getName() + " decrements: " + this.getValue());
+        } finally {
+            l.unlock();
         }
+        // }
+        //release a lock
     }
 }
 
@@ -41,7 +50,7 @@ public class F_synchronized {
         new Thread(counter, "Three").start();
         new Thread(counter, "Four").start();
 
-        Counter counter2=new Counter();
-        new Thread(counter2,"Counter 2").start();
+        Counter counter2 = new Counter();
+        new Thread(counter2, "Counter 2").start();
     }
 }
